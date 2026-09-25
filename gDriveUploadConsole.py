26e4f4d8163b2e375ab54340028c7ee34e15449e
Colin Janowicz <cjanowicz@gmail.com>
2026-09-24 23:16:31 -0700

    Initial Commit
    Private repository to track my work and make it easier to recover in case anything happens.

---
+++ gDriveUploadConsole.py
@@ -0,0 +1,64 @@
+
+import google.auth
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
+def upload_basic():
+
+    SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+    # creds, _ = login()
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", SCOPES
+        )
+        creds = flow.run_local_server(port=0)
+
+        # # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("drive", "v3", credentials=creds)
+        file_metadata = {'name': 'coffee_console.jpg'}
+
+        media = MediaFileUpload("coffee.jpg", mimetype="image/jpeg")
+
+        # pylint: disable=maybe-no-member
+
+        file = (
+            service.files()
+            .create(body=file_metadata, media_body=media, fields="id")
+            .execute()
+        )
+        print(f'File ID: {file.get("id")}')
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        file = None
+
+    return file.get("id")
+
+
+
+if __name__ == "__main__":
+    upload_basic()
+
+
